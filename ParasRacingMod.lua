b33ac8b5d00ab7979542389759e41ea29672fda3
paradigmsort <paradigmsort+hades@gmail.com>
2021-01-17 09:05:11 -0800

    Working version.
    Ensure we remember the "main" branch when we're in an abnormal room that
    the runner picked (eg. story, chaos, challenge) and edit subsequent
    room eligibility accordingly.

--- ParasRacingMod.lua
+++ ParasRacingMod.lua
@@ -1,11 +1,17 @@
-ModUtill.RegisterMod("ParasRacingMod")
+ModUtil.RegisterMod("ParasRacingMod")
 
 ParasRacingMod.FirstNormalRoom = nil
+ParasRacingMod.InPlaceOfRoom = nil
+ParasRacingMod.UsedRooms = {}
 
 ParasRacingMod.NonNormalRooms = {
   RoomSecret01 = true,
   RoomSecret02 = true,
   RoomSecret03 = true,
+  RoomChallenge01 = true,
+  RoomChallenge02 = true,
+  RoomChallenge03 = true,
+  RoomChallenge04 = true,
   A_Story01 = true,
   B_Story01 = true,
   C_Story01 = true,
@@ -22,14 +28,43 @@ function ParasRacingMod.IsNormalRoom(room)
   end
 end
 
-ModUtil.WrapBaseFunction("RandomSetNextInitSeed", function(baseFunc, ...)
+ModUtil.WrapBaseFunction("StartNewRun", function(baseFunc, ...)
+  local run = baseFunc(...)
   ParasRacingMod.FirstNormalRoom = nil
+  ParasRacingMod.InPlaceOfRoom = nil
+  ParasRacingMod.UsedRooms = {}
+  return run
+end, ParasRacingMod)
+
+ModUtil.WrapBaseFunction("StartRoom", function(baseFunc, ...)
+  if ParasRacingMod.FirstNormalRoom then
+    ParasRacingMod.InPlaceOfRoom = ParasRacingMod.FirstNormalRoom
+    table.insert( ParasRacingMod.UsedRooms, ParasRacingMod.FirstNormalRoom.Name )
+    ParasRacingMod.FirstNormalRoom = nil
+  end
+  return baseFunc(...)
+end, ParasRacingMod)
+
+ModUtil.WrapBaseFunction("RandomSetNextInitSeed", function(baseFunc, ...)
   RandomSynchronize()
-  baseFunc(...)
+  return baseFunc(...)
+end, ParasRacingMod)
+
+ModUtil.WrapBaseFunction("IsRoomEligible", function(baseFunc, currentRun, currentRoom, nextRoomData, args)
+  if Contains( ParasRacingMod.UsedRooms, nextRoomData.Name ) then
+    return false
+  elseif nextRoomData.GameStateRequirements
+    and nextRoomData.GameStateRequirements.RequiredFalseRooms
+    and ParasRacingMod.InPlaceOfRoom ~= nil
+    and Contains( roomData.GameStateRequirements.RequiredFalseRooms, ParasRacingMod.InPlaceOfRoom ) then
+    return false
+  else
+    return baseFunc(currentRun, currentRoom, nextRoomData, args)
+  end
 end, ParasRacingMod)
 
 ModUtil.WrapBaseFunction("CreateRoom", function(baseFunc, roomForDoorData, ...)
-  if IsNormalRoom(roomForDoorData) then
+  if ParasRacingMod.IsNormalRoom(roomForDoorData) then
     if ParasRacingMod.FirstNormalRoom == nil then
       ParasRacingMod.FirstNormalRoom = DeepCopyTable(roomForDoorData)
     else
